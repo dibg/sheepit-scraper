5d2a4dec80543bd79188ce2e17577da17a936bc0
dibg <bj2006s@hotmail.com>
2021-01-11 15:20:58 +0200

    seperated api from views

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from datetime import datetime
-from flask import Flask, jsonify, render_template, request
-from sqlalchemy import exc
-from flask_sqlalchemy import SQLAlchemy
-from configuration.credentials import POSTGRESQL_URI
+from flask_init import app
+from api import single_data_snapshot, db_data
+from flask import render_template, request
 from scraper.block import block_user
-from scraper.common_session_tasks import scrape_current_data, load_stored_data, get_valid_login_session
-from helper_functions.storage import store_as_json, retrieve_json
+from scraper.common_session_tasks import get_valid_login_session
 from helper_functions.file_filter import extract_routes_from_file
+
 
 # TODO: automatically scrape data
 # TODO: return redirect
@@ -14,44 +12,14 @@ from helper_functions.file_filter import extract_routes_from_file
 # TODO: document structure: header footer menu
 # TODO: filter database entries base on username, scene and date
 # TODO: handle exception for lost connection
-# FEATURE: user login page for blocking with different user than the one you scrape with
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = POSTGRESQL_URI
-db = SQLAlchemy(app)
-
-
-class Data(db.Model):
-    username = db.Column(db.String(64), nullable=False, primary_key=True)
-    scene = db.Column(db.String(200), nullable=False, primary_key=True)
-    frames = db.Column(db.Integer, nullable=False, primary_key=True)
-    devices = db.Column(db.String(12), nullable=False)
-    size = db.Column(db.String(18))
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def as_dict(self):
-        dict = {"username": self.username,
-                "scene": self.scene,
-                "frames": self.frames,
-                "devices": self.devices,
-                "size": self.size,
-                "date_created": self.date_created
-                }
-        return dict
-
-    def __init__(self, dict):
-        self.username = dict["username"]
-        self.scene = dict["scene"]
-        self.frames = dict["frames"]
-        self.devices = dict["devices"]
-        self.size = dict["size"]
+# TODO: user login page for blocking with different user than the one you scrape with
 
 
 @app.route("/")
 def index():
-    available_routes = extract_routes_from_file("app.py")
-    return render_template("index.html", available_routes=available_routes)
+    available_views = extract_routes_from_file("app.py")
+    available_api_routes = extract_routes_from_file("api.py")
+    return render_template("index.html", available_views=available_views, available_api_routes=available_api_routes)
 
 
 @app.route("/view_all")
@@ -80,73 +48,12 @@ def block():
     return "User " + username + " is blocked"
 
 
-@app.route("/v1/snapshot")
-def single_data_snapshot():
-    session = get_valid_login_session()
-    current_data = scrape_current_data(session)
-    data_json = jsonify(current_data)
-    return data_json
-
-
-@app.route("/v1/snapshot_to_db")
-def snapshot_to_db():
-    snapshot = single_data_snapshot()
-    for entry in snapshot.json:
-        data = Data(entry)
-        insert_data(data)
-    return "ok"
-
-
-@app.route("/v1/db_data")
-def db_data():
-    data = Data.query.all()
-    dict = []
-    for datum in data:
-        dict.append(datum.as_dict())
-    data_json = jsonify(dict)
-    return data_json
-
-
-@app.route("/v1/raw_data")
-def raw_data():
-    data = retrieve_json("data")
-    data_json = jsonify(data)
-    return data_json
-
-
-@app.route("/v1/load_stored_data_to_db")
-def load_data():
-    json_data = load_stored_data()
-    for entry in json_data:
-        data = Data(entry)
-        insert_data(data)
-    return "ok"
-
-
-def insert_data(entry):
-    try:
-        db_entry = Data.query.filter_by(username=entry.username, scene=entry.scene, frames=entry.frames).first()
-        if db_entry is not None:
-            db.session.delete(db_entry)
-            db.session.commit()
-        db.session.add(entry)
-        db.session.commit()
-    except exc.IntegrityError as e:
-        print(e.args[0])
-        db.session.rollback()
-
-
-@app.route("/v1/db_create")
-def create_db():
-    db.create_all()
-    return "ok"
-
-
-@app.route("/v1/db_delete")
-def delete_db():
-    Data.query.delete()
-    db.session.commit()
-    return "ok"
+# @app.before_first_request
+# def init_scheduler():
+#     scheduler = BackgroundScheduler()
+#     scheduler.add_job(func=snapshot_to_db, trigger="interval", minutes=1)
+#     scheduler.start()
+#     atexit.register(scheduler.shutdown)
 
 
 if __name__ == "__main__":
